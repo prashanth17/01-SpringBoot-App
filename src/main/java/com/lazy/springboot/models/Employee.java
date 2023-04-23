28a17e7b821fd231d89b54d3717347a5b182adcc
prashanth17 <prashant.muthoz@gmail.com>
2023-04-23 08:39:15 +0530

    added

---
+++ src/main/java/com/lazy/springboot/models/Employee.java
@@ -0,0 +1,32 @@
+package com.lazy.springboot.models;
+
+public class Employee {
+
+    private Integer id;
+    private String name;
+    private String gender;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
